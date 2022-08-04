e9fc642daa296c582a46fbe5700c80aa25a8adfd
evgenii <evgenii_08@list.ru>
2022-08-04 17:40:20 +0300

    Example007_Friend_and_Dog_2

--- Example007_Friend_and_Dog/Program.cs
+++ Example007_Friend_and_Dog/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите скорость первого друга: ");
+double firstFriendSpeed = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите скорость второго друга: ");
+double secondFriendSpeed = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите скорость собаки: ");
+double DogSpeed = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите расстояние между друзьями: ");
+double distance = Convert.ToDouble(Console.ReadLine());
+
+int count = 0;
+
+int friend = 2;
+
+double time = 0;
+
+while (distance > 5)
+{
+    if (friend == 1);
+    {
+        time = distance / (firstFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+     time = distance / (secondFriendSpeed + DogSpeed);
+     friend = 1;
+     
+   distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+   count = count + 1;
+}
+Console.Write("Собака пробежит раз: ");
+Console.WriteLine(count);
