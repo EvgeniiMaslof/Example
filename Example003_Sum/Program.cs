6609cf9e80b98833c9b03672aedd6a846812943a
evgenii <evgenii_08@list.ru>
2022-08-04 13:35:54 +0300

    Example003_Sum_enter_number

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,6 @@
-﻿int numberA = 5;
-int numberB = 8;
-int result = (numberA + numberB);
+﻿Console.Write("Напиши первое число: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Напиши второе число: ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+double result = (numberA + numberB);
 Console.Write(result);
-
