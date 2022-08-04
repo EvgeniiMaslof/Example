a245090dfed6414a461bfe9048ec651c76300cd6
evgenii <evgenii_08@list.ru>
2022-08-04 13:52:00 +0300

    Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿/onsole.Write("Напиши первое число: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Напиши второе число: ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+double result = (numberA / numberB);
+Console.Write(result);
