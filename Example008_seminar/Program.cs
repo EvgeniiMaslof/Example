02ee73b734e2f3eef24f56c917815b75df4a50d7
evgenii <evgenii_08@list.ru>
2022-08-06 15:28:59 +0300

    Example008_seminar

---
+++ Example008_seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstnumber>secondnumber)
+ { 
+    Console.WriteLine($"Максимальное значение: {firstnumber}");
+    Console.WriteLine($"Минимальное значение: {secondnumber}");
+ }
+else
+{
+    Console.WriteLine($"Максимальное значение: {secondnumber}");
+    Console.WriteLine($"Минимальное значение: {firstnumber}");
+}
