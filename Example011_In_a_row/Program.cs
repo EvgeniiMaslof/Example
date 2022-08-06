f074bf055471592a6bc087d331ff2f3bfb14255d
evgenii <evgenii_08@list.ru>
2022-08-06 16:35:50 +0300

    Example011_In_a_row

---
+++ Example011_In_a_row/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int ind = 2;
+
+while (ind <= number)
+{
+    Console.WriteLine(ind);
+    ind = ind + 2;
+}
+   
