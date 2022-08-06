1587b1875e1de9d8047c9b00d19843ce13d98ca8
evgenii <evgenii_08@list.ru>
2022-08-06 15:38:48 +0300

    Example009_max

---
+++ Example009_Max/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double number3 = Convert.ToDouble(Console.ReadLine());
+double max = number1;
+
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.Write($"Максимальное число: {max}");
