5c6de301744f2da3a682faf6fa518c2c535e30c8
evgenii <evgenii_08@list.ru>
2022-08-09 23:33:09 +0300

    the third_digit_of_the_number

---
+++ Example013/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введит число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 < 99)
+{
+    Console.Write("Третьей цифры нет");
+}
+else if (number1 < 999)
+{
+    number1 = (number1 % 10);
+    Console.Write($"Третья цифра: {number1}");
+}
+else if (number1 < 9999)
+{
+number1 = (number1 / 10) % 10;
+    Console.Write($"Третья цифра: {number1}");
+}
+else
+{
+    number1 = (number1 / 100) % 10;
+    Console.Write($"Третья цифра: {number1}");
+} 
