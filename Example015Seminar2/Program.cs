9b450eff6055855e40e626cb3c231b2ade6e8ed2
evgenii <evgenii_08@list.ru>
2022-08-13 00:43:27 +0300

    initial002

--- Example015Seminar2/Program.cs
+++ Example015Seminar2/Program.cs
@@ -6,7 +6,7 @@
 Console.Clear();
 Console.WriteLine("Введите пятизначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-if ((a / 10 > 999)&&(a < 99999))
+if ((a > 9999)&&(a < 99999))
 {
     if (a / 10000 == a % 10)
     {
