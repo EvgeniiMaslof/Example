c4c60853f4b158145d5d17275c86b07651e53c32
evgenii <evgenii_08@list.ru>
2022-08-15 22:24:17 +0300

    initial015

---
+++ Example015S/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+for (i = 1; i <= B; i++);
+Console.Write($"{Math.Pow(A, i)} ");
+
