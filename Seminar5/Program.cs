34ffdf8b7bc158ee5fccc19e3bec0771d8e04e5e
evgenii <evgenii_08@list.ru>
2022-08-21 18:18:45 +0300

    init6

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,31 +1,29 @@
-﻿// 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
-// которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+﻿// // 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// // которая покажет количество чётных чисел в массиве.
+// // [345, 897, 568, 234] -> 2
 
-
+Console.Clear();
 void arrayRandom(int [] array)
 {
-    int i = 0;
-   for(int length = array.Length; i < array.Length; i++);
-    {
-       array[i] = new Random().Next(0,10);
+   for (int i = 0; i < array.Length; i++)
+   {
+       array[i] = new Random().Next(100,1000);
        Console.Write($"{array[i]} ");
-    }
+   }
 }
 
-// int arrayChet(int[] array, int sum)
-// {
-//     int i = 0;
-//     int sum = 0;
-//     for(int i = 0; i < array.Length; i++);
-//     {
-//         if (array[i]% 2 == 0); sum++;
-//     }
-//     return Console.Write($"Количество четных чисел: {sum}");
-// }
+void arrayChet(int [] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+      if (array[i]% 2 == 0) sum++;
+    }
+    Console.WriteLine($"Количество четных чисел: {sum}");
+}
 
-Console.Write("Введите размер массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int [n];
+Console.Write("Задайте длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
 arrayRandom(array);
-// arrayChet(array);
+arrayChet(array);
