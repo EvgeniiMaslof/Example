85580fca7507b83c85d6545f885cafa8db6f63e8
evgenii <evgenii_08@list.ru>
2022-09-14 23:30:35 +0300

    NewMetod

--- Diplomnaja/Program.cs
+++ Diplomnaja/Program.cs
@@ -83,17 +83,15 @@ int length = Convert.ToInt32(Console.ReadLine());
 string[] array = new string [length];
 Console.WriteLine("Введите значение строки: ");
 int j = 0;
+
 for (int i = 0; i < array.GetLength(0); i++)
 {
     array[i] = Convert.ToString(Console.ReadLine());
     if(array[i].Length <= 3) j++;
 }
-Console.Write("[");
-for (int i = 0; i < array.GetLength(0); i++)
-{
-   Console.Write(array[i] + " ");
-}
-Console.Write("] - ");
+PrintArray(array);
+Console.Write(" - ");
+
 string[] array2 = new string [j];
 j = 0;
 for (int i = 0; i < array.GetLength(0); i++)
@@ -104,9 +102,14 @@ for (int i = 0; i < array.GetLength(0); i++)
         j++;
     }
 }
-Console.Write("[");
-for (int i = 0; i < array2.GetLength(0); i++)
+PrintArray(array2);
+
+void PrintArray(string [] array)  // Печатает массив
 {
-   Console.Write(array2[i] + " ");
+    Console.Write("[");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if(array[i]!= "") Console.Write(array[i]  + " ");
+    }
+    Console.Write("]");
 }
-Console.Write("]");
